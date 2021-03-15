d16347e2f3f90ba005f2d88e96db47fdbbbef9ae
MatricksDeCoder <infinitumcubed@gmail.com>
2021-03-15 09:15:17 +0200

    initial starter files

---
+++ src/components/Header/URLWarning.tsx
@@ -0,0 +1,23 @@
+//import React from 'react'
+import styled from 'styled-components'
+
+import { X } from 'react-feather'
+//import { isMobile } from 'react-device-detect'
+/*
+const PhishAlert = styled.div<{ isActive: any }>`
+  width: 100%;
+  padding: 6px 6px;
+  background-color: ${({ theme }) => theme.blue1};
+  color: white;
+  font-size: 11px;
+  justify-content: space-between;
+  align-items: center;
+  display: ${({ isActive }) => (isActive ? 'flex' : 'none')};
+`
+*/AnimationEffect
+
+export const StyledClose = styled(X)`
+  :hover {
+    cursor: pointer;
+  }
+`

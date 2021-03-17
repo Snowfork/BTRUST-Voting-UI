5ba66356540e99b5aefeb0ef1b74494cd70959f4
MatricksDeCoder <infinitumcubed@gmail.com>
2021-03-17 15:46:21 +0200

    fixed linter errors

--- ropsten-dummy-governance/deployed_contracts.js
+++ ropsten-dummy-governance/deployed_contracts.js
@@ -1,13 +1,9 @@
 //const { compABI } = JSON.parse(fs.readFileSync('Comp.json', 'utf8'))
-const compABI = require('./Comp.json')
-const govABI = require('./GovernorAlpha.json')
-const timelockABI = require('./Timelock.json')
-const {
-    encodeParameters,
-  } = require('../Utils/Ethereum');
+import compABI from './Comp.json'
+import govABI from './GovernorAlpha.json'
+import timelockABI from './Timelock.json'
+import encodeParameters from '../Utils/Ethereum'
 
-
-// used for dummy proposal 
 /*
 Proposal Function -> function propose(address[] memory targets, 
                                          uint[] memory values, 
@@ -27,13 +23,13 @@ const description = "This is a meaningless test proposal"
 
 const acct = '0xF94EeFdd507784260535D6B0777F7d7e661cCfe3'
 
-const info =  {
-    compAddress :'0xAe616e92935aBdE69BfFF95E675dF9CfD42ef227',
-    govAddress :'0x2a4Feed008D8BBD9a6eB29E8E0fEC5051131cee6',
-    timelockAddress: '0x881F16fe4F55CEe7D5D7890179531Ff8085a7167',
-    compABI,
-    govABI,
-    timelockABI
+const info = {
+  compAddress: '0xAe616e92935aBdE69BfFF95E675dF9CfD42ef227',
+  govAddress: '0x2a4Feed008D8BBD9a6eB29E8E0fEC5051131cee6',
+  timelockAddress: '0x881F16fe4F55CEe7D5D7890179531Ff8085a7167',
+  compABI,
+  govABI,
+  timelockABI
 }
 
 const callDatas = [encodeParameters(['address'], [acct])]
